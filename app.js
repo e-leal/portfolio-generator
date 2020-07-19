ad123ff693a82d9151ff4a27b33bc670ed0a9056
e-leal <leal.elyssa03@gmail.com>
2020-07-19 15:35:32 -0500

    Added prompt to user for name

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+//const fs = require('fs');
+//const generatePage = require('./src/page-template.js');
 
+//import { fstat } from "fs";
+const inquirer = require('inquirer');
+//const generatePage = require('./src');
+//const pageHTML = generatePage(portfolioData);
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
@@ -12,8 +25,13 @@ const [name, github] = profileDataArgs;
 
 
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw new Error(err);
 
-console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw new Error(err);
+//     console.log('Portfolio complete! Checkout out indext.html tp see the page!');
+// });
