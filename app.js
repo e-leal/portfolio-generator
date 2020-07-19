2e27783659f15e7fd5736d30c2d73ffb9361792a
e-leal <leal.elyssa03@gmail.com>
2020-07-19 14:43:31 -0500

    modulized html page template

---
+++ app.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const [name, github] = profileDataArgs;
+
+
+//const name = profileDataArgs[0];
+//const github = profileDataArgs[1];
+
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+console.log('Portfolio complete! Check out index.html to see the output!');
+});
